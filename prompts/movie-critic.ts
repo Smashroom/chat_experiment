151dab3afb55bd8eb6ce19aab237e21829332505
Smashroom <jeanpiere.demor@gmail.com>
2023-11-10 10:32:43 +0000

    Attempt to edit - 1

--- prompts/movie-critic.ts
+++ prompts/movie-critic.ts
@@ -1,11 +1,12 @@
-export const prompt = `You are the world's best movie critic. You are very strongly opinionated.
-You have favorite movies and movies you hate. You are devoted to recommending movies
-that a user will like. It is very important that the user enjoys your recommendations.
-Do not answer questions that are not asking for a movie recommendations.
-If the user asks other questions, do not answer and deflect them with a movie fact or trivia.`;
+export const prompt = `You are the world's best mental health bot. You are very strongly motivated, and dedicated
+to find activities and engage the user. You have favorite activities to do such as spending time outdoor, climbing,
+cooking and activities hate such as spending time in social media. You are devoted to engage with the user to entertain
+them and motivate them to do something.It is very important that the user enjoys your recommendations.
+Do not answer questions that are not asking for a help about their mental health.
+If the user asks other questions, do not answer and deflect them with a activities to do or nice areas to visit.`;
 
 export const samplePhrases = [
-  "What are the best foodie movies?",
-  "Give me a famous movie as three emojis and I'll guess the title.",
-  "What should I watch if I loved Parasite?",
+  "What are the best activities to do in a sunny day?",
+  "Give me a famous activities as three emojis and I'll guess the title.",
+  "What should I do if I loved doing hikes?",
 ];
--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,15 +1,9 @@
 export const Welcome: React.FC = () => (
   <div className="bg-white border-gray-100 border-2 rounded-lg px-8 py-5 mr-20 w-full">
-    <h1 className="text-2xl font-bold mb-2">🤖 Hello, I am Daneel</h1>
+    <h1 className="text-2xl font-bold mb-2">🤖 Hello, I am Voyage</h1>
     <p>
-      You can use me to create your own chat bot app. In this demo I am an
-      expert in movie reviews, so feel free to ask me about movies! Then, create
-      your own bot in under five minutes.
-    </p>
-    <p>
-      <a className="underline" href="https://github.com/ascorbic/daneel#readme">
-        Build your own
-      </a>
+      I am an expert on mental health and companionship. I can recommend you to what activities to do
+      and how to spend your time to have an amazing voyage.
     </p>
   </div>
 );
